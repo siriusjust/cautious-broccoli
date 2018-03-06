657056b22d9fb49e741794e56534087059da005b
Sean Chickosky <chickosky@gmail.com>
2018-03-06 03:01:08 +0000

    Minimum lambdas (i.e., won't fail on test)

--- fb_transform.py
+++ fb_transform.py
@@ -1,2 +1,6 @@
+import json, boto3, io
 def handler(event, context):
+    message = json.loads(event['Records'][0]['Sns']['Message'])
+    s3 = boto3.client('s3')
+    s3.upload_fileobj(io.BytesIO(message.encode()), 'meaningless-bucket', 'api_message')
     return None
