e8d162f5075a51ed3a42290c23d3c85dbe4abc21
siriusjust <siriusjjust@gmail.com>
2018-03-21 15:34:50 -0400

    Update fb_transform.py

--- fb_transform.py
+++ fb_transform.py
@@ -1,6 +1,7 @@
 import json, boto3, io
 def handler(event, context):
     message = json.loads(event['Records'][0]['Sns']['Message'])
+    message += "!"
     s3 = boto3.client('s3')
     s3.upload_fileobj(io.BytesIO(json.dumps(message).encode()), 'meaningless-bucket', 'api_message.txt')
     return None
